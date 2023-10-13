0bfdd2cfdad3ab7f82236cd718a6d675b0777607
Talha Gondal <145669194+talhagondal297@users.noreply.github.com>
2023-10-13 07:59:35 -0700

    User Input Added

---
+++ User Input Files/input1.py
@@ -0,0 +1,10 @@
+# taking input from user
+# By default user value is string
+number = input("Enter a number: ")
+print("You entered is: ",number)
+# For getting float value from user
+number = float(input("Enter a number: "))
+print("You entered is: ",number)
+#  For getting integer value from user
+number = int(input("Enter a number: "))
+print("You entered is: ",number)
---
+++ User Input Files/input2.py
@@ -0,0 +1,6 @@
+# Collecting Data
+name = input("Enter Your Name: ")
+age = input("Enter Your Age: ")
+city = input("Enter your city name: ")
+
+print(name + " is " + age + " Years Old! and he lives in " + city)
