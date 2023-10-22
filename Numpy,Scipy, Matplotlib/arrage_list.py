e3b55373a9877f5aa0ef5cf2db5873503193ab9d
Talha Gondal <145669194+talhagondal297@users.noreply.github.com>
2023-10-22 05:56:30 -0700

    Numpy, Scipy and Matplotlib Added

---
+++ Numpy,Scipy, Matplotlib/arrage_list.py
@@ -0,0 +1,16 @@
+# Import Libraries
+import numpy as np
+
+# Arrange list using np
+
+a = np.arange(1, 7)
+print(a)
+print(a.shape)
+
+b = np.arange(1, 15, 2)
+print(b)
+print(b.shape)
+
+c = np.arange(14, 2, -2)
+print(c)
+print(c.shape)
